85f566e058c0c9c071e4c61206de931c58db160e
Roxanne Lutz <rlutz1@unm,edu>
2025-09-02 17:24:37 -0600

    cleaning

--- Basic_f.m
+++
@@ -1,6 +0,0 @@
-function [output] = Basic_f(N)
-%Matlab function to account for the right handside of the ODE f(x,y)
-r=0.05;K=1000;
-%output=r*N*(1-N/K); %Logistic
-output=r*N; %basic exponential growth
-end
