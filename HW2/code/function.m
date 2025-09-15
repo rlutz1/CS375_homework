44ac05648c8c048163ab3dd68a0e1f0b48552a39
Roxanne Lutz <rlutz1@unm,edu>
2025-09-15 06:17:18 -0600

    adding a basic function file. because function is a keyword, renamed to function1. should probably mention that in read me or ask if they're going to be difficult about that.

--- HW2/code/function.m
+++
@@ -1,6 +0,0 @@
-% Roxanne Lutz
-% MATH375
-% 9/15/2025
-% HW2
-
-% this is the function file for hw 2
--- HW2/code/script.m
+++ HW2/code/script.m
@@ -2,3 +2,16 @@
 % MATH375
 % 9/15/2025
 % HW2
+
+% this is the main script file.
+
+% cleanup tasks
+clc, clearvars, close all;
+
+% testing
+%{
+[y, dy] = function1([3, 4, 5]);
+y, dy
+%}
+
+
