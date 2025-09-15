7081cae8c6ddff51d43be4f5e8ce6e5fcf0979bc
Roxanne Lutz <rlutz1@unm,edu>
2025-09-15 13:34:45 -0600

    cleaning

---
+++ HW2/functionWRONG.m
@@ -0,0 +1,13 @@
+% Roxanne Lutz
+% MATH375
+% 9/15/2025
+% HW2
+
+% this is the function file for hw 2
+% below is a reusable function for the given in the problem:
+% y = x^2
+% dy = 2x dx
+function [y, dy] = functionWRONG (x)
+    y = x .^ 2;
+    dy = x .* 2;
+end
