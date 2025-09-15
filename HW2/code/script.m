0dac5862c8a6c839f603752ceea45a4c029c79a8
Roxanne Lutz <rlutz1@unm,edu>
2025-09-15 07:10:36 -0600

    pushing the basic newtons method implementation for now before changing.

--- HW2/code/script.m
+++ HW2/code/script.m
@@ -15,3 +15,38 @@ y, dy
 %}
 
 
+% some initial values
+tolerance = 10 ^ (-4); % allowed to get approximate within 10^-4 as stated within problem.
+iterations = 20; % how many times to iterate
+x_0 = 1; % initial guess
+
+for i = 1:iterations;
+
+    [y, dy] = function1(x_0); % get f(x) and f'(x)
+
+    x = x_0 - (y ./ dy); % apply newtons method
+
+    xapprox(i) = x_0;
+    yapprox(i) = y;
+
+    if abs(x - x_0) < tolerance; % found 0 within tolerance
+        disp("Approximate found successfully: x = " + x);
+        %xapprox(i) = x;
+        %yapprox(i) = y;
+        break;
+    end;
+
+    x_0 = x;
+
+end;
+
+xexact = [-20:0.1:20];
+counter = 1;
+for x = xexact;
+    [y, dy] = function1(x);
+    yexact(counter) = y;
+    counter = counter + 1;
+end;
+
+
+plot(xexact, yexact, "--c")
