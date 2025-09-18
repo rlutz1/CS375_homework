003581a33e16533ef1b8e11d7e444c06f08bf63c
Roxanne Lutz <rlutz1@unm,edu>
2025-09-18 09:44:25 -0600

    just pushing the zip and final code here. readme is all good, everything is fine to submit.

---
+++ HW2/code/README.txt
@@ -0,0 +1,13 @@
+# MATH375 HW 2
+
+## script.m 
+
+This is the main script file to run. Running this has a default value of x0 = 2 (can be changed on line 14).
+
+Running this will bring up a plotting of the approximations against a plotting of the exact values of our function on a domain [-5, 5].
+
+## function1.m
+
+Please note: in the instructions it says to name this file `function.m`, but MATLAB appears to have issues with that since function is a keyword. Hence, .m file is `function1.m`.
+
+This is simply the coded version of our found by hand f(x), followed by f'(x), also found by a simple derivative.
--- HW2/code/script.m
+++ HW2/code/script.m
@@ -15,10 +15,14 @@ x_curr = 2; % initial guess
 
 % flag for a successful approximation within these parameters
 success = false;
+% for counting the actual iterations used for easy printing
+iterationsNeeded = 0;
 
 for i = 1:iterations;
 
-    [y, dy] = function2(x_curr); % get f(x) and f'(x)
+    iterationsNeeded = iterationsNeeded + 1; % for printing only
+
+    [y, dy] = function1(x_curr); % get f(x) and f'(x)
 
     % hold values for plotting
     xapprox(i) = x_curr;
@@ -35,22 +39,42 @@ end;
 
 % print success if we've found a zero within iterations and tolerance
 if success;
-    disp("Approximate root found successfully: x = " + x_curr);
+    disp( ...
+        "Approximate root found successfully in " ...
+        + iterationsNeeded ...
+        + " iterations: x = " ...
+        + x_curr ...
+        );
 else;
-    disp("Approximate root was NOT found within tolerance in " + iterations + " iterations.");
+    disp( ...
+        "Approximate root was NOT found within tolerance in " ...
+        + iterations ...
+        + " iterations." ...
+        );
 end;
 
-% plot exact values of function to find root of
+% get exact values of function to find root of
 h = 0.1; startPt = -5; endPt =  5; % initilization values
 xexact = [startPt:h:endPt]; % calucluate exact for these vals of x
 yexact = zeros(size(xexact)); % preallocate memory
 counter = 1;
 
 for x = xexact;
-    [y, dy] = function2(x);
+    [y, dy] = function1(x);
     yexact(counter) = y;
     counter = counter + 1;
 end;
 
 % plot the exact graph versus the approximation points
-plot(xexact, yexact, "--c", xapprox, yapprox, "oy")
+figure(1);
+plot(xexact, yexact, "--c", xapprox, yapprox, "oy", LineWidth=1.5);
+hold on;
+
+legend( ...
+    'Exact Values of f(x)', ...
+    'Approximations to the Root' ...
+    );
+title('Derived f(x) vs Approximations to the Root r, where f(r) = 0');
+xlabel('Domain, [-5, 5] with h = 0.1');
+ylabel('Range, output of f(x) and Approximations of f(r) = 0');
+grid on;
