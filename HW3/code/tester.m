d30f98dcc7685fc41fee58487aab48ea41a93ee1
Roxanne Lutz <rlutz1@unm,edu>
2025-10-07 06:52:41 -0600

    zip file of final code

--- HW3/code/tester.m
+++
@@ -1,8 +0,0 @@
-% not to submit, for playing with and testing theory on saddling issues
-
-function [y, dy] = tester(x)
-    % convergence point problem at sqrt(3) for this
-    y = (x .^ 2) / (1 + (x .^ 2));
-    dy = (2 * x) / ((1 + (x .^ 2)) .^ 2);
-
-end
