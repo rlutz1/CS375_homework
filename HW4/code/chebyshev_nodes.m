248712c96d2ec3f7c1571cd1a8a6961c2d236fc6
Roxanne Lutz <rlutz1@unm,edu>
2025-10-13 14:46:21 -0600

    just pushing comments, noise on 60 has not been diagnosed

--- HW4/code/chebyshev_nodes.m
+++ HW4/code/chebyshev_nodes.m
@@ -6,13 +6,13 @@
 % function to produce chebyshev nodes for interpolation
 % on a generic interval given a number of nodes.
 
-function [x_G10] = chebyshev_nodes(num_nodes, start_pt, end_pt)
+function [x_GN] = chebyshev_nodes(num_nodes, start_pt, end_pt)
 
 x_k = @(k) cos(((k + 0.5) .* pi) ./ num_nodes);
 x_k_tilde = @(k) (0.5 .* (start_pt + end_pt)) + (0.5 .* (end_pt - start_pt) * x_k(k));
 
 for k = 0:(num_nodes - 1);
-    x_G10(k + 1) = x_k_tilde(k);
+    x_GN(k + 1) = x_k_tilde(k);
 end;
 
 end
