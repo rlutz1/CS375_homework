d301a302f05e3d67fb72504612c7eab65555c928
Roxanne Lutz <rlutz1@unm,edu>
2025-10-13 12:45:43 -0600

    plotting of runge exact with 10000 points on [-5, 5]. confirmed with desmos that i wrote function correctly, fucking yeesh.

--- HW4/code/script_01.m
+++ HW4/code/script_01.m
@@ -3,6 +3,38 @@
 % 10/13/2025
 % HW4
 
+% script run cleanup tasks
+clc, clearvars, close all;
+
+% preliminary: get f(x) plotted with more than 11 points
+start_pt = -5; end_pt = 5;
+h = 0.001; % 10,000 points plotted
+x_exact = [start_pt:h:end_pt];
+
+i = 1;
+for x = x_exact;
+    y_exact(i) = runge_function(x);
+    i = i + 1;
+end;
+
+% plot the exact graph only, mainly for ensuring code is correct
+figure(1);
+plot( ...
+    x_exact, y_exact, "--c", ...
+    LineWidth=1.5 ...
+    );
+hold on;
+
+legend( ...
+    'Exact Values of Runge Function' ...
+    );
+title('Exact Values ONLY of Runge Function');
+xlabel("Domain, [" + start_pt + ", " + end_pt + "] with h = " + h);
+ylabel('Output of Runge Function');
+grid on;
+
 % -----------------------------------------------------
 % PART A
 % -----------------------------------------------------
+
+
