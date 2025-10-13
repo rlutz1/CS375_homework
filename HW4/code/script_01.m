3c491abf1682327ff9da13978eda5c786b85eeaa
Roxanne Lutz <rlutz1@unm,edu>
2025-10-13 14:27:54 -0600

    plotting of part D (and C), but note that G60 has some weird noise to it. unsure if that's intentional or not yet.

--- HW4/code/script_01.m
+++ HW4/code/script_01.m
@@ -37,7 +37,7 @@ grid on;
 
 % -----------------------------------------------------
 % PART A
-% + construct and plot P_10 on [-5, 5] using equispaced pts
+% + construct and plot P10 on [-5, 5] using equispaced pts
 % -----------------------------------------------------
 
 n = 10; % P_10, 11 equispaced nodes (n + 1 nodes)
@@ -56,7 +56,7 @@ end;
 figure(2);
 plot( ...
     x_exact, y_exact, "--c", ...
-    x_P10, y_P10, 'go', ...
+    x_P10, y_P10, 'bo', ...
     x_P10_Interp, y_P10_Interp, "--y", ...
     LineWidth=1.5 ...
     );
@@ -67,14 +67,14 @@ legend( ...
     "Data Points", ...
     "Interpolation" ...
     );
-title('Runge Exact VS P_10 Interpolation');
+title('Runge Exact VS P10 Interpolation');
 xlabel("Domain, [" + start_pt + ", " + end_pt + "] with h = " + h);
 ylabel('Output of Runge Function and Interpolation');
 grid on;
 
 % -----------------------------------------------------
 % PART B
-% + construct and plot G_10 on [-5, 5] using equispaced pts
+% + construct and plot G10 on [-5, 5] using Chebyshev nodes
 % -----------------------------------------------------
 
 x_G10 = chebyshev_nodes(n + 1, start_pt, end_pt)
@@ -91,8 +91,8 @@ end;
 figure(3);
 plot( ...
     x_exact, y_exact, "--c", ...
-    x_G10, y_G10, 'go', ...
-    x_G10_Interp, y_G10_Interp, "--y", ...
+    x_G10, y_G10, 'bo', ...
+    x_G10_Interp, y_G10_Interp, "--g", ...
     LineWidth=1.5 ...
     );
 hold on;
@@ -102,11 +102,105 @@ legend( ...
     "Data Points", ...
     "Interpolation" ...
     );
-title('Runge Exact VS G_10 Interpolation');
+title('Runge Exact VS G10 Interpolation');
 xlabel("Domain, [" + start_pt + ", " + end_pt + "] with h = " + h);
 ylabel('Output of Runge Function and Interpolation');
 grid on;
 
 
+% -----------------------------------------------------
+% PART C
+% + Plot all 3 (exact, P10, G10) together
+% -----------------------------------------------------
+
+figure(4);
+plot( ...
+    x_exact, y_exact, "--c", ...
+    x_P10_Interp, y_P10_Interp, "--y", ...
+    x_G10_Interp, y_G10_Interp, "--g", ...
+    LineWidth=1.5 ...
+    );
+hold on;
+
+legend( ...
+    'Exact Values of Runge Function', ...
+    "Interpolation with Equispaced Points (P10)", ...
+    "Interpolation with Chebyshev Nodes (G10)" ...
+    );
+title('Runge Exact VS P10, G10 Interpolation');
+xlabel("Domain, [" + start_pt + ", " + end_pt + "] with h = " + h);
+ylabel('Output of Runge Function and Interpolation');
+grid on;
+
+% -----------------------------------------------------
+% PART D
+% + construct and plot G20, G40, G60 on [-5, 5] using equispaced
+% + fix window to x = [-5, 5], y = [-0.5, 2]
+% -----------------------------------------------------
+
+% --------------------
+n = 20;
+x_G20 = chebyshev_nodes(n + 1, start_pt, end_pt);
+y_G20 = runge_function(x_G20); % evaluate data points
+
+a_coeff = Coef(n, x_G20, y_G20); % get coefficients
+
+% evaluate the interpolating polynomial at a finer mesh
+x_G20_Interp = [start_pt:h:end_pt];
+for i = 1:length(x_G20_Interp);
+    y_G20_Interp(i) = Eval(n, x_G20, a_coeff, x_G20_Interp(i));
+end;
 
 
+% -----------------
+n = 40;
+x_G40 = chebyshev_nodes(n + 1, start_pt, end_pt);
+y_G40 = runge_function(x_G40); % evaluate data points
+
+a_coeff = Coef(n, x_G40, y_G40); % get coefficients
+
+% evaluate the interpolating polynomial at a finer mesh
+x_G40_Interp = [start_pt:h:end_pt];
+for i = 1:length(x_G40_Interp);
+    y_G40_Interp(i) = Eval(n, x_G40, a_coeff, x_G40_Interp(i));
+end;
+
+% ------------------------
+
+% -----------------
+n = 60;
+x_G60 = chebyshev_nodes(n + 1, start_pt, end_pt);
+y_G60 = runge_function(x_G60); % evaluate data points
+
+a_coeff = Coef(n, x_G60, y_G60); % get coefficients
+
+% evaluate the interpolating polynomial at a finer mesh
+x_G60_Interp = [start_pt:h:end_pt];
+for i = 1:length(x_G60_Interp);
+    y_G60_Interp(i) = Eval(n, x_G60, a_coeff, x_G60_Interp(i));
+end;
+
+% ------------------------
+
+figure(5);
+plot( ...
+    x_exact, y_exact, "--c", ...
+    x_G20_Interp, y_G20_Interp, "--r", ...
+    x_G40_Interp, y_G40_Interp, "--y", ...
+    x_G60_Interp, y_G60_Interp, "--g", ...
+    LineWidth=1.5 ...
+    );
+hold on;
+
+legend( ...
+    'Exact Values of Runge Function', ...
+    "Interpolation with Chebyshev Nodes (G20)", ...
+    "Interpolation with Chebyshev Nodes (G40)", ...
+    "Interpolation with Chebyshev Nodes (G60)" ...
+    );
+title('Runge Exact VS G20, G40, G60 Interpolation');
+xlabel("Domain, [" + start_pt + ", " + end_pt + "] with h = " + h);
+xlim([-5, 5]);
+ylabel('Output of Runge Function and Interpolation');
+ylim([-0.5, 2]);
+grid on;
