8a8e1775c114db7519e819c34edafeb562b3d100
Roxanne Lutz <rlutz1@unm,edu>
2025-11-03 17:39:18 -0700

    cleaning :)

--- HW5/code/script.m
+++ HW5/code/script.m
@@ -6,16 +6,22 @@
 % script run cleanup tasks
 clc, clearvars, close all;
 
+% -------------------------------------
+% PRELIMINARY
+%
+% plot exact Runge for verification
+% -------------------------------------
+
 % preliminary: get f(x) plotted with a fine mesh
 start_pt = -5; end_pt = 5;
 h = 0.001; % 10,000 points plotted
-x_exact = [start_pt:h:end_pt];
-y_exact1 = runge_function(x_exact);
+x_exact_init = [start_pt:h:end_pt];
+y_exact_init = runge_function(x_exact_init);
 
 % plot the exact graph only, mainly for ensuring code is correct
 figure('Name', 'Exact Runge');
 plot( ...
-    x_exact, y_exact1, "--c", ...
+    x_exact_init, y_exact_init, "--c", ...
     LineWidth=1.5 ...
     );
 hold on;
@@ -36,82 +42,100 @@ grid on;
 % -------------------------------------
 
 % SPLINE WITH 21 KNOTS
+
 % number of equally spaced nodes
 n = 21;  %s_21
 %spacing
 h = (end_pt - start_pt) / (n - 1);
-t_knots = [start_pt:h:end_pt]; % knot vector
-y_table_values = runge_function(t_knots); % fill the table
+t_knots_21 = [start_pt:h:end_pt]; % knot vector
+y_table_values_21 = runge_function(t_knots_21); % fill the table
 
-x_finer1 = [start_pt:h/5:end_pt]; %choose a finer grid to plot points in between knots
+x_finer_21 = [start_pt:(h / 5):end_pt]; %choose a finer grid to plot points in between knots
 %call function, X=evaluation of an interval or point
-S_21 = spline_deg_1(t_knots, y_table_values, x_finer1);
+S_21 = spline_deg_1(t_knots_21, y_table_values_21, x_finer_21);
 
 % -------------------------------------
 
 % SPLINE WITH 41 KNOTS
+
 % number of equally spaced nodes
 n = 41;  %s_41
 %spacing
 h = (end_pt - start_pt) / (n - 1);
-t_knots = [start_pt:h:end_pt]; % knot vector
-y_table_values = runge_function(t_knots); % fill the table
+t_knots_41 = [start_pt:h:end_pt]; % knot vector
+y_table_values_41 = runge_function(t_knots_41); % fill the table
 
-x_finer2 = [start_pt:h/5:end_pt]; %choose a finer grid to plot points in between knots
+x_finer_41 = [start_pt:(h / 5):end_pt]; %choose a finer grid to plot points in between knots
 %call function, X=evaluation of an interval or point
-S_41 = spline_deg_1(t_knots, y_table_values, x_finer2);
+S_41 = spline_deg_1(t_knots_41, y_table_values_41, x_finer_41);
 
 % -------------------------------------
 
 % SPLINE WITH 81 KNOTS
+
 % number of equally spaced nodes
 n = 81;  %s_81
 %spacing
 h = (end_pt - start_pt) / (n - 1);
-t_knots = [start_pt:h:end_pt]; % knot vector
-y_table_values = runge_function(t_knots); % fill the table
+t_knots_81 = [start_pt:h:end_pt]; % knot vector
+y_table_values_81 = runge_function(t_knots_81); % fill the table
 
-x_finer3 = [start_pt:h/5:end_pt]; %choose a finer grid to plot points in between knots
+x_finer_81 = [start_pt:(h / 5):end_pt]; %choose a finer grid to plot points in between knots
 %call function, X=evaluation of an interval or point
-S_81 = spline_deg_1(t_knots, y_table_values, x_finer3);
+S_81 = spline_deg_1(t_knots_81, y_table_values_81, x_finer_81);
 
 % -------------------------------------
 
 % plot the splines
 figure('Name', 'S_21, S_41, S_81');
 plot( ...
-    x_finer1, S_21, '--r' , ...
-    x_finer2, S_41, '--y' , ...
-    x_finer3, S_81, '--g'  ...
-    ... % t_knots, y_table_values, 'rx' ...
+    x_finer_21, S_21, '--r' , ...
+    x_finer_41, S_41, '--y' , ...
+    x_finer_81, S_81, '-g',  ...
+    ... t_knots_21, y_table_values_21, '*r', ...
+    ... t_knots_41, y_table_values_41, '+y', ...
+    ... t_knots_81, y_table_values_81, 'og', ...
+    LineWidth=1 ...
+ )
+hold on;
+
+legend( ...
+    'S21', 'S41', 'S81'... , ...
+    ... "S21 Table Vals", "S41 Table Vals", "S81 Table Vals" ...
     )
-legend('S21', 'S41', 'S81')
-title(['Degree 1 Spline Approximation, ', 'n = 21, 41, 81'])
+title('Degree 1 Spline Approximation, n = 21, 41, 81');
+xlabel("Domain, [" + start_pt + ", " + end_pt + "]");
+ylabel('Approximate Runge Function');
+grid on;
 
 % -------------------------------------
 
-% gettinng and plotting error for analysis
-y_exact1 = runge_function(x_finer1);
-error1 = abs(y_exact1 - S_21);
+% getting and plotting error for analysis
+y_exact_21 = runge_function(x_finer_21);
+error_21 = abs(y_exact_21 - S_21);
 
-y_exact2 = runge_function(x_finer2);
-error2 = abs(y_exact2 - S_41);
+y_exact_41 = runge_function(x_finer_41);
+error_41 = abs(y_exact_41 - S_41);
 
-y_exact3 = runge_function(x_finer3);
-error3 = abs(y_exact3 - S_81);
+y_exact_81 = runge_function(x_finer_81);
+error_81 = abs(y_exact_81 - S_81);
 
 figure('Name', 'Absolute Error of Spline Approximations'); %plotting the error
 plot( ...
-    x_finer1, error1, '.-r', ...
-    x_finer2, error2, '.-y', ...
-    x_finer3, error3, '.-g' ...
+    x_finer_21, error_21, '.-r', ...
+    x_finer_41, error_41, '.-y', ...
+    x_finer_81, error_81, '.-g' ...
     );
+hold on;
 legend( ...
     "Error of S21", ...
     "Error of S41", ...
     "Error of S81" ...
     );
-title(['Degree 1 Spline Error, ','n = 21, 41, 81'])
+title('Degree 1 Spline Error, n = 21, 41, 81');
+xlabel("Domain, [" + start_pt + ", " + end_pt + "]");
+ylabel('|Exact - Approximate|');
+grid on;
 
 % -------------------------------------
 % PART B
@@ -142,18 +166,18 @@ end;
 %spacing
 % h = (end_pt - start_pt) / (n - 1);
 t_knots = x_G20; % knot vector, chebyshev
-y_table_values = runge_function(t_knots); % fill the table
+y_table_values_21 = runge_function(t_knots); % fill the table
 
-x_finer1 = [start_pt:0.1:end_pt]; %choose a finer grid to plot points in between knots
+x_finer_21 = [start_pt:0.1:end_pt]; %choose a finer grid to plot points in between knots
 %call function, X=evaluation of an interval or point
-S_21 = spline_deg_1(t_knots, y_table_values, x_finer1);
+S_21 = spline_deg_1(t_knots, y_table_values_21, x_finer_21);
 
 % -------------------------------------
 
 % plot the spline and G20
 figure('Name', 'S_21, S_20');
 plot( ...
-    x_finer1, S_21, '--c' , ...
+    x_finer_21, S_21, '--c' , ...
     x_G20_Interp, y_G20_Interp, '--y' ...
     ... % t_knots, y_table_values, 'rx' ...
     )
